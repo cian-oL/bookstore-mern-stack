5b819a2b6d740329eab0ea7d1db780160610bff4
mecastor <mecastor87@gmail.com>
2023-09-06 15:44:53 +0100

    Created react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import CreateBooks from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import ShowBook from "./pages/ShowBook";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">App</h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBooks />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+      <Route path="books/edit/:id" element={<EditBook />} />
+      <Route path="books/details/:id" element={<ShowBook />} />
+    </Routes>
   )
 }
 export default App
