84e75023fdde40c94320e6e87a26fd0a05df9413
mecastor <mecastor87@gmail.com>
2023-09-06 12:07:38 +0100

    Added custom CORS policy

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ import express, { request } from "express";
 import { PORT, mongoDbUrl } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS Policy with custom options
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   console.log(req);
